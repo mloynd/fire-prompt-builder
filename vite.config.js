3fa92f1c33856f1a90622ecffee5af8b296cbe6a
mloynd <mike.loynd@councilfire.ai>
2025-06-14 22:59:08 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(async () => {
+  const react = await import('@vitejs/plugin-react').then(m => m.default)
+  return {
+    plugins: [react()]
+  }
 })
